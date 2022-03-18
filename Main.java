e57dcee30f7b2688acce3a13d6cdc05375a3bbad
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-03-18 14:05:40 +0530

    added:part time employee logic

--- Main.java
+++ Main.java
@@ -6,17 +6,22 @@ public class Main {
         public static void main(String[] args) {
             int PER_HOUR_WAGE = 20;
             int WORKING_HOURS = 0;
-
-            double random = Math.floor(Math.random() * 10) % 2;
-            if(random == 1){
+            int IS_FULL_TIME_EMP = 1;
+            int IS_PART_TIME_EMP = 2;
+            double random = Math.floor(Math.random() * 10) % 3;
+            if(random == IS_FULL_TIME_EMP){
                 WORKING_HOURS = 8;
-                System.out.println("Employee is present");
+                System.out.println("full time Employee is present");
+            }
+            else if(random == IS_PART_TIME_EMP) {
+                WORKING_HOURS = 8;
+                System.out.println("Part time employee is present");
             }
             else{
                 WORKING_HOURS = 0;
                 System.out.println("Employee is absent");
             }
             int daily_wage = PER_HOUR_WAGE * WORKING_HOURS;
-            System.out.println("today wage is: " + daily_wage);
+            System.out.println("today wage is : " + daily_wage);
     }
 }
