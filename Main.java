453f49b1a35042adf3377ac12bd5f298d94a3f38
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-03-18 16:30:03 +0530

    added:while logic

--- Main.java
+++ Main.java
@@ -3,31 +3,36 @@ package com.company;
 
 public class Main {
 
-        public static void main(String[] args) {
-            int PER_HOUR_WAGE = 20;
-            int WORKING_HOURS = 0;
-            final int IS_FULL_TIME_EMP = 1;
-            final int IS_PART_TIME_EMP = 2;
-            int monthly_hours = 0;
-            for (int i = 1; i <= 20; i++) {
-                int random = (int) Math.floor(Math.random() * 10) % 3;
-                switch (random) {
+    public static void main(String[] args) {
+        int PER_HOUR_WAGE = 20;
+        int WORKING_HOURS = 0;
+        final int IS_FULL_TIME_EMP = 1;
+        final int IS_PART_TIME_EMP = 2;
+        int monthly_hours = 0;
+        int total_working_days = 1;
 
-                    case IS_FULL_TIME_EMP:
-                        WORKING_HOURS = 8;
-                        System.out.println("Full time employee is present");
-                        break;
-                    case IS_PART_TIME_EMP:
-                        WORKING_HOURS = 8;
-                        System.out.println("Part time employee is present");
-                        break;
-                    default:
-                        WORKING_HOURS = 0;
-                        System.out.println("Employee is absent");
-                }
-                monthly_hours = monthly_hours + WORKING_HOURS;
+        while (monthly_hours <=100 || total_working_days <=20){
+            int random = (int) Math.floor(Math.random() * 10) % 3;
+            switch (random) {
+
+                case IS_FULL_TIME_EMP:
+                    WORKING_HOURS = 8;
+                    System.out.println("Full time employee is present");
+                    total_working_days++;
+                    break;
+                case IS_PART_TIME_EMP:
+                    WORKING_HOURS = 8;
+                    System.out.println("Part time employee is present");
+                    total_working_days++;
+                    break;
+                default:
+                    WORKING_HOURS = 0;
+                    System.out.println("Employee is absent");
             }
-            int monthly_wage = PER_HOUR_WAGE * monthly_hours;
-            System.out.println("today wage is : " + monthly_wage);
+            monthly_hours = monthly_hours + WORKING_HOURS;
+        }
+        int monthly_wage = PER_HOUR_WAGE * monthly_hours;
+        System.out.println("monthly wage is: " + monthly_wage);
+
     }
 }
