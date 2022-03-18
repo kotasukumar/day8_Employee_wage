e65c2df4f9abe844306c47287126e7efdcb93be3
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-03-18 13:48:15 +0530

    added: attence logic

--- Main.java
+++ Main.java
@@ -4,6 +4,12 @@ package com.company;
 public class Main {
 
         public static void main(String[] args) {
-
+            double random = Math.floor(Math.random() * 10) % 2;
+            if(random == 1){
+                System.out.println("Employee is present");
+            }
+            else{
+                System.out.println("Employee is absent");
+            }
     }
 }
