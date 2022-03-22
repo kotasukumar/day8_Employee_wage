fd96865b46d6c78952f239ff2a6b128d7c8edd61
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-03-22 18:16:24 +0530

    added: class and object

--- Main.java
+++ Main.java
@@ -4,17 +4,16 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int PER_HOUR_WAGE = 20;
-        int WORKING_HOURS = 0;
         final int IS_FULL_TIME_EMP = 1;
         final int IS_PART_TIME_EMP = 2;
+        int PER_HOUR_WAGE = 20;
+        int WORKING_HOURS = 0;
         int monthly_hours = 0;
         int total_working_days = 1;
 
-        while (monthly_hours <=100 || total_working_days <=20){
+        while (monthly_hours <= 100 || total_working_days <= 20) {
             int random = (int) Math.floor(Math.random() * 10) % 3;
             switch (random) {
-
                 case IS_FULL_TIME_EMP:
                     WORKING_HOURS = 8;
                     System.out.println("Full time employee is present");
@@ -30,9 +29,8 @@ public class Main {
                     System.out.println("Employee is absent");
             }
             monthly_hours = monthly_hours + WORKING_HOURS;
+            int monthly_wage = PER_HOUR_WAGE * monthly_hours;
+            System.out.println("monthly wage is: " + monthly_wage);
         }
-        int monthly_wage = PER_HOUR_WAGE * monthly_hours;
-        System.out.println("monthly wage is: " + monthly_wage);
-
     }
 }
